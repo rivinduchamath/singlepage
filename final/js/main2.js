f67c3981afcbe4ae606507f12ad447c9b63f452e
Rivindu Chamath <41599441+rivinduchamath@users.noreply.github.com>
2019-08-04 09:48:01 +0530

    Add files via upload

---
+++ final/js/main2.js
@@ -0,0 +1,9 @@
+
+$(".container").on("click",function() { var t = ( $(".container").id());
+   if(t == 'q1'){
+       css('display','#home');
+   }
+    if(t == 'q2'){
+        css('display','#customers');
+    }
+});
